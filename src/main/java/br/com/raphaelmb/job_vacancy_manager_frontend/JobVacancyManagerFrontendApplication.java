934bfaf304f9e185ffea117110293453bcac3c03
Raphael Berger <raphaelmb@gmail.com>
2025-06-25 13:59:51 -0300

    project setup

---
+++ src/main/java/br/com/raphaelmb/job_vacancy_manager_frontend/JobVacancyManagerFrontendApplication.java
@@ -0,0 +1,13 @@
+package br.com.raphaelmb.job_vacancy_manager_frontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobVacancyManagerFrontendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobVacancyManagerFrontendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/raphaelmb/job_vacancy_manager_frontend/JobVacancyManagerFrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.raphaelmb.job_vacancy_manager_frontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobVacancyManagerFrontendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
