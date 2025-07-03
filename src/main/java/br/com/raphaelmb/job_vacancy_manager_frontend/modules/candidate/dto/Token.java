6e2826fedb3855477279fb8601cf42142bcdde03
Raphael Berger <raphaelmb@gmail.com>
2025-07-02 22:32:53 -0300

    candidate login

---
+++ src/main/java/br/com/raphaelmb/job_vacancy_manager_frontend/modules/candidate/dto/Token.java
@@ -0,0 +1,13 @@
+package br.com.raphaelmb.job_vacancy_manager_frontend.modules.candidate.dto;
+
+public class Token {
+    private String access_token;
+
+    public String getAccess_token() {
+        return access_token;
+    }
+
+    public void setAccess_token(String access_token) {
+        this.access_token = access_token;
+    }
+}
---
+++ src/main/java/br/com/raphaelmb/job_vacancy_manager_frontend/modules/candidate/service/CandidateService.java
@@ -0,0 +1,31 @@
+package br.com.raphaelmb.job_vacancy_manager_frontend.modules.candidate.service;
+
+import br.com.raphaelmb.job_vacancy_manager_frontend.modules.candidate.dto.Token;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class CandidateService {
+    public Token login(String username, String password) {
+        RestTemplate rt = new RestTemplate();
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        Map<String, String> data = new HashMap<>();
+        data.put("username", username);
+        data.put("password", password);
+
+        HttpEntity<Map<String, String>> request = new HttpEntity<>(data, headers);
+
+        var result = rt.postForObject("http://localhost:8080/candidate/auth", request, Token.class);
+
+        return result;
+    }
+}
